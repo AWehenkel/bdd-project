fa276c57cb714dd47731a5dcc8adc0f2f67a08a4
AWehenkel <antoine.wehenkel@student.ulg.ac.be>
2016-04-23 16:19:28 +0200

    Fin Q2A

---
+++ fonctionnel.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["id"])){
+    header("location: connexion.php");
+}
+if(isset($_GET["disconnect"])){
+    session_destroy();
+    header("location: connexion.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Interface de la base de données</title>
+    <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+</head>
+<body>
+<?php
+include_once "DBRequester.php";
+$bdd = new DBRequester();
+?>
+<div class = "center">
+    <div class = "menu-selection">
+        <h2>Exemplaires fonctionnels</h2>
+    </div>
+    <div class = "result-box">
+        <div id = "result_box">
+        <br/><label for = "table_name" ></label><?php $bdd->printTable(NULL, 0, 10, "*", false, $bdd->getFonctionnel()); ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
