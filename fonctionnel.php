dfd4703ad773d29c58446ca71d1481eab737ae57
sebj02 <sjoris@student.ulg.ac.be>
2016-04-23 14:14:18 +0200

    Added files via upload
    Q2c

---
+++ fonctionnel.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["id"])){
+    header("location: connexion.php");
+}
+if(isset($_GET["disconnect"])){
+    session_destroy();
+    header("location: connexion.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Interface de la base de données</title>
+    <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+</head>
+<body>
+<?php
+include_once "db_requester.php";
+$bdd = new db_requester();
+?>
+<div class = "center">
+    <div class = "menu-selection">
+        <h2>Exemplaires fonctionnels</h2>
+    </div>
+    <div class = "result-box">
+        <div id = "result_box">
+        </br><label for = "table_name" ></label><?php $bdd->print_table(NULL, 0, 10, "*", false, $bdd->getFonctionnel()); ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
